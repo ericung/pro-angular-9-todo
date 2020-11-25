6b9b83422ecd89bc0a239adf470e206247e4a06b
Eric Ung <ungericw@tutanota.com>
2020-11-25 11:36:59 -0600

    11/25/2020 added show complete todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,11 +22,18 @@ export class AppComponent {
   }
 
   get itemCount(): number {
-    return this.list.items
-      .filter(item => !item.complete).length;
+    return this.items.length;
   }
 
   get items(): readonly TodoItem[] {
-    return this.list.items;
+    return this.list.items.filter(item => this.showComplete || !item.complete);
   }
+
+  addItem(newItem) {
+    if (newItem != "") {
+        this.list.addItem(newItem);
+    }
+  }
+
+  showComplete: boolean = false;
 }
